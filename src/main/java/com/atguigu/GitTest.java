b41dbf052205a8ee3ad3b2ef9c765549c96eb225
SSZZPP123 <81072136+SSZZPP123@users.noreply.github.com>
2021-09-02 11:37:57 +0800

    pullTest

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master Test");
         System.out.println("Hot-fix test!");
         System.out.println("pushtest!");
+        System.out.println("GitHubTest");
     }
 }
